405d941a13efebb735eec7a11a289f21d89d1b14
Armel <armelfli50@gmail.com>
2019-11-12 22:40:49 -0600

    Done.

--- index.js
+++ index.js
@@ -11,18 +11,16 @@ function submitData(name, email){
         body: JSON.stringify({name: name, email: email})
     };
     fetch("http://localhost:3000/users",params)
-    .then(resp => resp.json())
-    .then(json => {addNewuser(json);return fetch("http://localhost:3000/users",params)
-})
-    .catch(e => {document.body.append("Unauthorized Access")})//;return  fetch("http://localhost:3000/users",params)})
-//    return fetch("http://localhost:3000/users",params)
+    .then(resp => { if (!resp.ok) { throw resp };return resp.json()})
+    .then(json => {addNewuser(json); return fetch("http://localhost:3000/users",params)})
+    .catch(e => {document.body.append("Unauthorized Access")})
 }
-
+// return fetch("http://localhost:3000/users",params
 function addNewuser(body){
-        document.querySelector("body").append(body.id)
+
         document.querySelector("body").append(" " + body.email)
-        
         document.querySelector("body").append(" " + body.name)
+        document.querySelector("body").append(body.id)
 
-
+        body.id
 }
--- test/indexTest.js
+++ test/indexTest.js
@@ -53,24 +53,24 @@ describe( "submitData()", () => {
       .to.eq( "steve@steve.com" )
   } )
 
-  // it( "handles the POST request response, retrieves the new id value and appends it to the DOM", async function () {
-  //   nock( 'http://localhost:3000' )
-  //     .post( '/users' )
-  //     .reply( 201, function ( uri, requestBody ) {
-  //       return {
-  //         id: rando,
-  //         ...requestBody
-  //       }
-  //     } );
+  it( "handles the POST request response, retrieves the new id value and appends it to the DOM", async function () {
+    nock( 'http://localhost:3000' )
+      .post( '/users' )
+      .reply( 201, function ( uri, requestBody ) {
+        return {
+          id: rando,
+          ...requestBody
+        }
+      } );
 
-  //   let name = "Sam"
-  //   let email = "sam@sam.com"
+    let name = "Sam"
+    let email = "sam@sam.com"
 
-  //   await submitData( name, email )
+    await submitData( name, email )
 
-  //   expect( document.body.innerHTML )
-  //     .to.include( rando )
-  // } );
+    expect( document.body.innerHTML )
+      .to.include( rando )
+  } );
 
   it( "handles a failed POST request using catch, appends the error message to the DOM", async function () {
     let message = 'Unauthorized Access'

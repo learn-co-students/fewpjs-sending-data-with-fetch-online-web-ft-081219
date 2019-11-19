3c096c6e70e9e6f4b692232103ecd34635544abc
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-18 17:33:50 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch('http://localhost:3000/users', configObj)
+  .then(resp => resp.json())
+  .then(json => document.body.innerHTML = json[ "id" ])
+  .catch(e => document.body.innerHTML = e.message)
+};

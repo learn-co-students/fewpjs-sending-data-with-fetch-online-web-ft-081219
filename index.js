9c45fdcf7916ca82084b2239410d376471f67fd6
KHong <albtrossfacefall@gmail.com>
2019-11-29 11:39:03 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+function appendToDom(a) {
+  document.body.append(a)
+};
+
+function submitData(userNameStr, emailStr) {
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: userNameStr, email: emailStr})
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(response => {return response.json();})
+    .then(function(object) {
+      appendToDom(object.id);
+    })
+    .catch(function(error) {
+      document.body.append(error.message);
+    });
+    
+};
